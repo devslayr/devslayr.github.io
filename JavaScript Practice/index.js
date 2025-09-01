8736f005d529481b6ee6513f1bd88ae985f9aebb
Nguyen Minh Duc <s4125574@rmit.edu.vn>
2025-09-01 17:05:23 +0700

    Update

---
+++ JavaScript Practice/index.js
@@ -0,0 +1,16 @@
+// console.log(`Hello`);
+// console.log('I like pizza');
+
+// window.alert("This is an alert!");
+// window.alert(`I like Pizza`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like Pizza`;
+
+
+//This is a comment
+
+/* this
+is 
+a 
+comment */
